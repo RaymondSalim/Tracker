c89c0bd1ca56a72e3fb484a4d6c05c8414317bb5
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-07-29 00:04:04 +0700

    Initial upload of Tracker app - Added OutgoingCallHander service that extends CallRedirectionService     - Checks if a specific number is dialed, cancels the call and launches MainActivity

---
+++ app/src/main/java/com/reas/tracker/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.reas.tracker
+
+import android.app.role.RoleManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    val REQUEST_ID: Int = 1;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        requestRole()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun requestRole() {
+        var roleManager: RoleManager = getSystemService(Context.ROLE_SERVICE) as RoleManager
+        var intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_REDIRECTION)
+        startActivityForResult(intent, REQUEST_ID)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ID) {
+            Log.d("TAG", "onActivityResult: app is call redirection app")
+        } else {
+            Log.d("TAG", "onActivityResult: nope")
+        }
+    }
+}
---
+++ app/src/main/java/com/reas/tracker/OutgoingCallHandler.kt
@@ -0,0 +1,43 @@
+package com.reas.tracker
+
+import android.app.role.RoleManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.net.Uri
+import android.telecom.Call
+import android.telecom.CallRedirectionService
+import android.telecom.PhoneAccountHandle
+import android.util.Log
+
+class OutgoingCallHandler : CallRedirectionService() {
+
+    override fun onPlaceCall(
+        handle: Uri,
+        initialPhoneAccount: PhoneAccountHandle,
+        allowInteractiveResponse: Boolean
+    ) {
+        var blockCall: Boolean = false
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+
+        // Number that is dialed
+        var phoneNumber: String = handle.toString()
+        phoneNumber = phoneNumber.replace("tel:", "")
+
+        if (phoneNumber.equals("**2506**")) {
+            // Cancels the call
+            cancelCall()
+
+            // Launches MainActivity
+            startActivity(intent)
+        }
+
+
+
+    }
+
+
+}
